329b7ae12220f00be8f85799d77213f99be2dba8
TheAlmightyCrumb <sagiv.keeper@gmail.com>
2021-11-16 22:44:54 +0200

    Wait test using 3 different types

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,14 +1,19 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     private static ChromeDriver driver;
+    private static WebDriverWait wait;
 
     @BeforeClass
     public static void initialise() {
@@ -17,16 +22,17 @@ public class Main {
         driver.manage().window().maximize();
     }
 
+    /* Ex. 1 */
     @Test
     public void waitsTest() {
         /* Using implicit wait */
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://dgotlieb.github.io/Selenium/synchronization.html");
-//        driver.findElementByCssSelector("button#btn").click();
-//        WebElement hiddenText = driver.findElement(By.cssSelector("p#message"));
-//        System.out.println(hiddenText.getText());
+        driver.findElementByCssSelector("button#btn").click();
+        WebElement hiddenText = driver.findElement(By.cssSelector("p#message"));
+        System.out.println(hiddenText.getText());
 
-        /* Usin thread.sleep */
+        /* Using thread.sleep */
         try {
             driver.findElement(By.cssSelector("button#hidden")).click();
             Thread.sleep(4000);
@@ -35,6 +41,12 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        /* Using explicit wait */
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.findElement(By.cssSelector("button#rendered")).click();
+        WebElement afterRenderClick =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#finish2")));
+        System.out.println(afterRenderClick.getText());
     }
 
     @AfterClass
